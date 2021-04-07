0e91f4baa3bbaa2c890c6c1b610065edf081b020
Sullivan <adressecours0.1@gmail.com>
2021-04-07 17:59:26 +0200

    winning / losing logic implemented + score display + reset / next word button

--- game.js
+++ game.js
@@ -1,68 +1,68 @@
-const numberOflives = 6;
+let numberOflives = 6;
+let correctGuess = [];
+let poolOfWords = ['confusion', 'classic', 'potential', 'excellent', 'glacial', 'trust', 'bittersweet', 'noise', 'polite', 'famous', 'trial','emotion','strike','crude', 'coalition']
 
 
-//button = 
-{/* <button id="new-game">
-  New game
-  </button>
-  CSS
-  
-  #new-game {
-    background: #43a3e6;
-    border: 1px solid #43a3e6;
-    border-radius: 6px;
-    box-shadow: 1px 1px 2px #999;
-    color: #fff;
-    padding: 6px 14px;
-    outline: 0;
-  } */}
 
-
-let pool = ['confusion', 'classic', 'potential', 'excellent', 'glacial', 'trust', 'bittersweet', 'noise', 'polite', 'famous', 'trial','emotion','strike','crude', 'coalition']
-
-let randomWord = pool[Math.floor(Math.random() * pool.length)]
-console.log(randomWord);
-
-let correctGuess = []
-//1/ If the letter is within the randomWord , then it would be pushed into correct guess
-//using includes
+let currentWord = poolOfWords[Math.floor(Math.random() * poolOfWords.length)]
+console.log(currentWord);
 
 let keys = document.querySelectorAll('.letter-style');
 
 for(let i = 0; i < keys.length ; i++){
     keys[i].addEventListener('click', function(){
-        if(randomWord.includes(keys[i].innerText.toLowerCase()) && !correctGuess.includes(keys[i].innerText.toLowerCase())) {
+        if(currentWord.includes(keys[i].innerText.toLowerCase()) && !correctGuess.includes(keys[i].innerText.toLowerCase())) {
             correctGuess.push(keys[i].innerText.toLowerCase())
+        } else if (!currentWord.includes(keys[i].innerText.toLowerCase())){
+            numberOflives -= 1;
         }
         display();
+        liveCounter();
         console.log(correctGuess);
+        console.log(numberOflives);
     })
+}
+
+function liveCounter (){
+    if(numberOflives < 1) window.alert('You lose!');
+}
+
+function reset(){
+    numberOflives = 6;
+    correctGuess = [];
 }
 
 function display(){
 
-    //Display should be nested within a "new word" button
-    //in order to trigger the dashes before trying to guess a word (add event listener) ?
-    
-
     let placeHolder = '';
 
-    for (let i = 0; i < randomWord.length; i++){
-        if(correctGuess.includes(randomWord[i])){
-            placeHolder += `${randomWord[i]} `
+    for (let i = 0; i < currentWord.length; i++){
+        if(correctGuess.includes(currentWord[i])){
+            placeHolder += `${currentWord[i]} `
         } else {
             placeHolder += '_ '
         }
     }
     let placeHolderSpace = document.querySelector('.placeholder')
-    placeHolderSpace.innerHTML = placeHolder;  
-    
+    placeHolderSpace.innerHTML = placeHolder;
+    if(currentWord === placeHolderSpace.innerHTML.replace(/\s/g, '')) window.alert('You won!')
+
 }
 
 
+let button = document.querySelector('#new-game').addEventListener('click', function(){
+    currentWord = getNewWord(poolOfWords, currentWord)
+    display();
+    console.log(currentWord)
+})
 
-//having the charactere + the remaining dashes
-//checking the length of the string
-//keep track of how many time the users press something
-
-
+function getNewWord(wordsArray, currentWord){
+    const randomIndex = Math.floor((Math.random() * wordsArray.length));
+    const chosenWord = wordsArray[randomIndex];
+ 
+    if (chosenWord === currentWord) {
+        return getNewWord();
+    }
+    reset();
+    return chosenWord;
+ }
