21032db2bd78f689dfab61a258da998e614afdda
Sullivan <adressecours0.1@gmail.com>
2021-04-08 18:06:15 +0200

    mvp

--- game.js
+++ game.js
@@ -3,6 +3,41 @@ let correctGuess = [];
 let poolOfWords = ['confusion', 'classic', 'potential', 'excellent', 'glacial', 'trust', 'bittersweet', 'noise', 'polite', 'famous', 'trial','emotion','strike','crude', 'coalition']
 let total = 0;
 
+let div3 = document.querySelector('.div3');
+
+document.querySelector('.trashtalk').style.display = 'none';
+
+let head = document.querySelector('.head');
+head.style.display = "none";
+
+let body = document.querySelector('.body')
+body.style.display = "none";
+
+let leftArm = document.querySelector('.left-arm')
+leftArm.style.display = "none";
+
+let rightArm = document.querySelector('.right-arm')
+rightArm.style.display = "none";
+
+let leftLeg = document.querySelector('.left-leg')
+leftLeg.style.display = "none";
+
+let rightLeg = document.querySelector('.right-leg')
+rightLeg.style.display = "none";
+
+//this could be an object ?
+
+let thrashtalk = ["Lol, can't you spell ? ", "Really ? Repeat after me : ", "Please open your dictionnary and go to : "];
+let randomTT = thrashtalk[Math.floor(Math.random() * thrashtalk.length)]
+console.log(randomTT)
+let thrashtalkDiv = document.querySelector('.trashtalk')
+
+
+let kudos = ['Nice ! Praised be the Genius Guesser ', 'Bravo ! Your sublime intellect has unveiled the thruth ', 'Damn ! You broke the game']
+let kudosRand = kudos[Math.floor(Math.random() * kudos.length)]
+console.log(kudosRand)
+let kudosRandDiv = document.querySelector('.trashtalk');
+
 let currentWord = poolOfWords[Math.floor(Math.random() * poolOfWords.length)]
 console.log(currentWord);
 
@@ -10,14 +45,52 @@ let keys = document.querySelectorAll('.letter-style');
 
 let totalPoints = document.querySelector('.score');
 
+//
+
 for(let i = 0; i < keys.length ; i++){
     keys[i].addEventListener('click', function(){
         if(currentWord.includes(keys[i].innerText.toLowerCase()) && !correctGuess.includes(keys[i].innerText.toLowerCase())) {
             correctGuess.push(keys[i].innerText.toLowerCase());
-            // keys[i].setAttribute('disabled', '');
+            keys[i].style.background = 'green';
         } else if (!currentWord.includes(keys[i].innerText.toLowerCase())){
             numberOflives -= 1;
-            // keys[i].setAttribute('disabled', '');
+            keys[i].style.background = 'red';
+            switch (numberOflives) {
+                case 5:
+                    head = document.querySelector('.head')
+                    head.style.display = "block";
+                    break;
+                case 4:
+                    head = document.querySelector('.head').style.display = "block";
+                    body = document.querySelector('.body').style.display = "block";
+                    break;
+                case 3:
+                    head = document.querySelector('.head').style.display = "block";
+                    body = document.querySelector('.body').style.display = "block";
+                    leftArm = document.querySelector('.left-arm').style.display = "block";
+                    break;
+                case 2:
+                    head = document.querySelector('.head').style.display = "block";
+                    body = document.querySelector('.body').style.display = "block";
+                    leftArm = document.querySelector('.left-arm').style.display = "block";
+                    rightArm = document.querySelector('.right-arm').style.display = "block";
+                    break;
+                case 1: 
+                    head = document.querySelector('.head').style.display = "block";
+                    body = document.querySelector('.body').style.display = "block";
+                    leftArm = document.querySelector('.left-arm').style.display = "block";
+                    rightArm = document.querySelector('.right-arm').style.display = "block";
+                    leftLeg = document.querySelector('.left-leg').style.display = "block";
+                    break;
+                case 0:
+                    head = document.querySelector('.head').style.display = "block";
+                    body = document.querySelector('.body').style.display = "block";
+                    leftArm = document.querySelector('.left-arm').style.display = "block";
+                    rightArm = document.querySelector('.right-arm').style.display = "block";
+                    leftLeg = document.querySelector('.left-leg').style.display = "block";
+                    rightLeg = document.querySelector('.right-leg').style.display = "block";
+                    break;
+                }
         }
         display();
         livesCounter();
@@ -26,7 +99,12 @@ for(let i = 0; i < keys.length ; i++){
 
 function livesCounter (){
     if(numberOflives < 1){
-        window.alert(`You lose! Your score is ${total}`);
+        thrashtalkDiv.innerText = randomTT + `${currentWord}`;
+        thrashtalkDiv.style.display = 'block';
+            thrashtalkDiv.style.fontSize = '40px';
+            thrashtalkDiv.style.fontStyle = 'sans-serif';
+            thrashtalkDiv.style.color = 'black';
+        document.querySelector('.div3').style.display = "none";
         reset();
         total = 0;
         totalPoints.innerHTML = total;
@@ -36,6 +114,10 @@ function livesCounter (){
 function reset(){
     numberOflives = 6;
     correctGuess = [];
+    keys = document.querySelectorAll('.letter-style')
+    keys.forEach(function(key){
+        key.style.background = '#43a3e6';
+    })
 }
 
 function display(){
@@ -52,17 +134,29 @@ function display(){
     let placeHolderSpace = document.querySelector('.placeholder')
     placeHolderSpace.innerHTML = placeHolder;
     if(currentWord === placeHolderSpace.innerHTML.replace(/\s/g, '')) {
-        window.alert('Well play!');
+        document.querySelector('.trashtalk').innerText = kudosRand;
+        document.querySelector('.div3').style.display = "none";
+        kudosRandDiv.style.display = 'block';
+        //change the kudos style
         total++;
         totalPoints.innerHTML = total;
     }
 }
 
-
-let button = document.querySelector('#new-game').addEventListener('click', function(){
+document.querySelector('#new-game').addEventListener('click', function(){
     currentWord = getNewWord(poolOfWords, currentWord)
     display();
+    // randomT();
+    // kudosR();
     console.log(currentWord)
+    document.querySelector('.div3').style.display = "flex";
+    document.querySelector('.trashtalk').style.display = 'none';
+    document.querySelector('.head').style.display = "none";
+    document.querySelector('.body').style.display = "none";
+    document.querySelector('.left-arm').style.display = "none";
+    document.querySelector('.right-arm').style.display = "none";
+    document.querySelector('.left-leg').style.display = "none";
+    document.querySelector('.right-leg').style.display = "none";
 })
 
 function getNewWord(wordsArray, currentWord){
@@ -75,3 +169,13 @@ function getNewWord(wordsArray, currentWord){
     reset();
     return chosenWord;
  }
+
+
+//  function kudosR(){
+//      return kudosRand;
+//  }
+
+//  function randomT(){
+//      return randomTT;
+//  }
+
